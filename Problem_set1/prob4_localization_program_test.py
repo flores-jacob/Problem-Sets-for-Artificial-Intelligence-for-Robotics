d18f8846af1897f0b99ee3a8671c866eb7205a50
phwebmachine <phwebmachine@gmail.com>
2018-01-12 16:10:20 +0800

    test: Write test code for instances when sense_right is less than 1.0

--- Problem_set1/prob4_localization_program_test.py
+++ Problem_set1/prob4_localization_program_test.py
@@ -28,7 +28,22 @@ class LocalizeTest(unittest.TestCase):
              [0.0, 0.0, 0.0]])
         self.assertEqual(sense(colors, measurement, sensor_right), correct_answer)
 
-
+    def test_sense_two_R_with_error(self):
+        colors = [['G', 'G', 'G'],
+                  ['G', 'R', 'R'],
+                  ['G', 'G', 'G']]
+        measurement = 'R'
+        sensor_right = 0.8
+        correct_answer = (
+            [[0.06666666666, 0.06666666666, 0.06666666666],
+             [0.06666666666, 0.26666666666, 0.26666666666],
+             [0.06666666666, 0.06666666666, 0.06666666666]])
+        answer = sense(colors, measurement, sensor_right)
+        for row_index in range(len(answer)):
+            for tile_index in range(len(answer[row_index])):
+                difference = answer[row_index][tile_index] - correct_answer[row_index][tile_index]
+                # make sure that the difference between the elements is negligible
+                self.assertLess(difference, abs(.000000001))
 
     def test_localize(self):
         pass
